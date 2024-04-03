cf5c887cd7a0b40a8e3d856a9494ade3dddc90ca
Kartik Kittad <99243266+Kartikkittad@users.noreply.github.com>
2024-04-03 10:13:26 +0530

    Add files via upload

--- modules/schema.js
+++ modules/schema.js
@@ -10,28 +10,44 @@ const airData = new Schema({
     "URL": String
 });
 
+const conData = new Schema({
+    "Container Company Name": String,
+    "URL": String,
+    "Prefix": String,
+    "Track URL": String
+})
+
+const favoriteSchema = new Schema({
+    email: String,
+    awbNumber: String,
+})
 
 const UserSchema = new Schema({
-    email: String,
-    username: String,
-    password: {
-        type: String,
-        required: true,
-        minlength: 6
-    },
-    isEmailVerified: { type: Boolean, default: false }
+    name: String,
+    designation: String,
+    companyName: String,
+    email: { type: String, unique: true },
+    mobileno: String,
+    password: String,
+    isVerified: { type: Boolean, default: false },
+    verificationToken: String,
 });
 
 const trackedAwbSchema = new Schema({
-    awbNumber: String
+    awbNumber: String,
+    airlineName: String,
 });
 
 const TrackedAwbModel = mongoose.model('TrackedAwb', trackedAwbSchema);
 const UserModel = mongoose.model('users', UserSchema)
 const MyModel = mongoose.model('awbNumber', airData);
+const Favorite = mongoose.model('Favorite', favoriteSchema);
+const Container = mongoose.model('containers', conData);
 
 module.exports = {
     UserModel,
     MyModel,
-    TrackedAwbModel
+    TrackedAwbModel,
+    Favorite,
+    Container,
 };
